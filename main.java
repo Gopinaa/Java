5e25293e53323a33857818922eb8d58be32a063c
Gopinaa <tatagopinath@gmail.com>
2022-02-13 19:13:53 +0530

    main class modified

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class main{
 	public static void main(String args[]) {
 		child c=new child();
-		int sum=c.add(10,20);
+		int sum=c.add(40,20);
 		System.out.println(sum);
 		
 	}
